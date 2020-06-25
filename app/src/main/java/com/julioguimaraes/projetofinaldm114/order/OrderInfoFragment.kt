8d3d6373bbd15cd26a846287106cf7e3510b9754
Júlio César Campos Guimarães <julio.guimaraes@msn.com>
2020-06-25 10:28:58 -0300

    Inicial

--- app/src/main/java/com/julioguimaraes/projetofinaldm114/order/OrderInfoFragment.kt
+++ app/src/main/java/com/julioguimaraes/projetofinaldm114/order/OrderInfoFragment.kt
@@ -1,21 +1,11 @@
 package com.julioguimaraes.projetofinaldm114.order
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.ktx.Firebase
-import com.julioguimaraes.projetofinaldm114.R
 import com.julioguimaraes.projetofinaldm114.databinding.FragmentOrderInfoBinding
 import com.julioguimaraes.projetofinaldm114.persistence.Order
 import com.squareup.moshi.JsonAdapter
